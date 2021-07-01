55e085541662ce4c97c2b987cd4c261ca9bf8165
ahdernasr <44983175+ahdernasr@users.noreply.github.com>
2021-07-01 17:12:35 +0100

    Environment variables

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 import todoRoutes from './routes/todos.js'
 
 const app = express();
+dotenv.config()
 
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
@@ -13,10 +15,9 @@ app.use(cors());
 
 app.use('/todos', todoRoutes)
 
-const CONNECTION_URL = `mongodb+srv://ahmed:ahmed123@cluster0.jtfyg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {
+mongoose.connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
